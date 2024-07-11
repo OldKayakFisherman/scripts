f1bf4482850dd9484715fd4db3932ca3b3a1101f
OldKayakFisherman <94559756+OldKayakFisherman@users.noreply.github.com>
2024-07-11 08:57:44 -0400

    Update startJenkinsDocker.sh

--- startJenkinsDocker.sh
+++ startJenkinsDocker.sh
@@ -37,7 +37,7 @@ docker run \
   --env DOCKER_HOST=tcp://docker:2376 \
   --env DOCKER_CERT_PATH=/certs/client \
   --env DOCKER_TLS_VERIFY=1 \
-  --publish 8080:8080 \
+  --publish 8004:8080 \
   --publish 50000:50000 \
   --volume jenkins-data:/var/jenkins_home \
   --volume jenkins-docker-certs:/certs/client:ro \
